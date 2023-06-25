e93f67e3cf4777d9ddc4f8752f1d257bbf438294
intellild <intellild@outlook.com>
2023-06-25 13:32:45 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'sdk',
+      // the proper extensions will be added
+      fileName: 'sdk',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  }
+})
